52aa1d98313d475fd1480c301511caf9960e173e
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-19 00:03:15 +0530

    Connected to mongo

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import connectToMongo from "./db.js";
+connectToMongo();
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
