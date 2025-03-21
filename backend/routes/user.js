33f5398e6118ffa9de0136dd864209bf3cc52c09
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-21 17:10:20 +0530

    Get user API added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,14 +5,35 @@ const userRouter = express.Router();
 //Create user
 userRouter.post("/createuser", async (req, res) => {
   try {
-    const user = await UserSchema.create({
-      userName: req.body.name,
-      userEmail: req.body.email,
-      userPassword: req.body.password,
-      userGroups: req.body?.userGroups,
-      userExpenses: req.body?.userExpenses,
-      date: Date.now(),
+    let user = await UserSchema.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({
+        success,
+        error: "Email already exists",
+      });
+    } else {
+      const user = await UserSchema.create({
+        userName: req.body.name,
+        userEmail: req.body.email,
+        userPassword: req.body.password,
+        userGroups: req.body?.userGroups,
+        userExpenses: req.body?.userExpenses,
+        date: Date.now(),
+      });
+      res.send(user);
+    }
+  } catch (error) {
+    return res.status(500).json({
+      error: "Server error",
+      message: error.message,
     });
+  }
+});
+
+//Get All Users
+userRouter.get("/getallusers", async (req, res) => {
+  try {
+    const user = await UserSchema.find();
     res.send(user);
   } catch (error) {
     return res.status(500).json({
@@ -23,9 +44,9 @@ userRouter.post("/createuser", async (req, res) => {
 });
 
 //Get User
-userRouter.get("/getallusers", async (req, res) => {
+userRouter.get("/getuser/:id", async (req, res) => {
   try {
-    const user = await UserSchema.find();
+    const user = await UserSchema.findById(req.params.id);
     res.send(user);
   } catch (error) {
     return res.status(500).json({
