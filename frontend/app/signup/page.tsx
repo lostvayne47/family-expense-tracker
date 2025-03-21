941e3533596cee4c0398e07060f23f19da88439a
Yash Dhanore <yashdhanore@gmail.com>
2025-03-22 00:47:50 +0100

    sign up page set up

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { FormEvent } from "react";
 
+// TODO: fix use client -> maybe make form a separate component
 export default function Signup() {
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -26,8 +27,21 @@ export default function Signup() {
     }
   }
 
+  // TODO: check later
+  //   function confirmPassword(event: FormEvent<HTMLInputElement>): boolean {
+  //     const password = (document.getElementById("password") as HTMLInputElement)
+  //       .value;
+  //     const confirmPassword = event.currentTarget.value;
+  //     if (password !== confirmPassword) {
+  //       event.currentTarget.setCustomValidity("Passwords do not match");
+  //     } else {
+  //       event.currentTarget.setCustomValidity("");
+  //     }
+  //     return password !== confirmPassword;
+  //   }
+
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
@@ -83,10 +97,16 @@ export default function Signup() {
                   name="confirm-password"
                   id="confirm-password"
                   placeholder="••••••••"
+                  //   onChange={confirmPassword}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
-              <button type="submit">Submit</button>
+              <button
+                type="submit"
+                className="w-full text-white bg-black hover:bg-gray-900 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Create an account
+              </button>
             </form>
           </div>
         </div>
