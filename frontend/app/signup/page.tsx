1a0e3b040f6ec02ddf1901b5119f13f80e48401b
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-22 20:12:40 +0530

    error fixed

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -137,7 +137,6 @@ export default function Signup() {
               </div>
               <button
                 type="submit"
-                disabled={isFormValid}
                 className="w-full text-white bg-black hover:bg-gray-900 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 Create an account
