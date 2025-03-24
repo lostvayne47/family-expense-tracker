8ef7e96c7e227cca39ae8852a978ce37a2582d62
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-24 22:37:03 +0530

    Redirect to home if auth token is present

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTheme } from "./ThemeProvider";
 import { useNavigate } from "react-router";
 const LandingPage = () => {
   const { darkMode } = useTheme();
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const authToken = localStorage.getItem("auth-token");
+    if (authToken) {
+      navigate("/home");
+    }
+  }, []);
   function handleClick(event) {
     const location = event.target.name;
     if (location) {
