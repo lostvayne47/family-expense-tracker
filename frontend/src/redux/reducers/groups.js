a924c640d0ad1209461eddc0161cc89a528c13fe
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-25 10:46:22 +0530

    reducer updated

--- frontend/src/redux/reducers/groups.js
+++ frontend/src/redux/reducers/groups.js
@@ -5,7 +5,7 @@ import {
 } from "../actions/groups.js";
 
 const initialState = {
-  group: null, // Will store user data
+  groups: null, // Will store user data
   groupLoading: false, // Tracks API loading state
   error: null, // Stores any errors
 };
@@ -15,7 +15,7 @@ const groupReducer = (state = initialState, action) => {
     case FETCH_GROUP_REQUEST:
       return { ...state, groupLoading: true, error: null }; // Set loading state
     case FETCH_GROUP_SUCCESS:
-      return { ...state, group: action.payload, groupLoading: false }; // Store user data
+      return { ...state, groups: action.payload, groupLoading: false }; // Store user data
     case FETCH_GROUP_FAILURE:
       return { ...state, groupLoading: false, error: action.payload }; // Store error message
     default:
