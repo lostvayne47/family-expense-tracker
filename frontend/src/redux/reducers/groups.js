6b056f57338c42018e72493e5a867627ae297c3a
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-25 10:43:24 +0530

    Group reducer keys edited

--- frontend/src/redux/reducers/groups.js
+++ frontend/src/redux/reducers/groups.js
@@ -6,18 +6,18 @@ import {
 
 const initialState = {
   group: null, // Will store user data
-  loading: false, // Tracks API loading state
+  groupLoading: false, // Tracks API loading state
   error: null, // Stores any errors
 };
 
 const groupReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_GROUP_REQUEST:
-      return { ...state, loading: true, error: null }; // Set loading state
+      return { ...state, groupLoading: true, error: null }; // Set loading state
     case FETCH_GROUP_SUCCESS:
-      return { ...state, user: action.payload, loading: false }; // Store user data
+      return { ...state, group: action.payload, groupLoading: false }; // Store user data
     case FETCH_GROUP_FAILURE:
-      return { ...state, loading: false, error: action.payload }; // Store error message
+      return { ...state, groupLoading: false, error: action.payload }; // Store error message
     default:
       return state;
   }
--- frontend/src/redux/reducers/user.js
+++ frontend/src/redux/reducers/user.js
@@ -6,18 +6,18 @@ import {
 
 const initialState = {
   user: null, // Will store user data
-  loading: false, // Tracks API loading state
+  userLoading: false, // Tracks API loading state
   error: null, // Stores any errors
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_USER_REQUEST:
-      return { ...state, loading: true, error: null }; // Set loading state
+      return { ...state, userLoading: true, error: null }; // Set loading state
     case FETCH_USER_SUCCESS:
-      return { ...state, user: action.payload, loading: false }; // Store user data
+      return { ...state, user: action.payload, userLoading: false }; // Store user data
     case FETCH_USER_FAILURE:
-      return { ...state, loading: false, error: action.payload }; // Store error message
+      return { ...state, userLoading: false, error: action.payload }; // Store error message
     default:
       return state;
   }
