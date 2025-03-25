e9906277cb50fc4f154bce78e3322d9f8d07bc20
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-25 15:21:13 +0530

    Padding and gap adjusted

--- frontend/src/components/UserView.js
+++ frontend/src/components/UserView.js
@@ -10,42 +10,40 @@ export default function UserView({ userData, groups }) {
   return (
     <div className="h-100 flex  items-center justify-center text-gray-300 overflow-auto">
       <div className=" h-100 w-full max-w-md bg-gray-800 shadow-lg rounded-lg p-3  border border-gray-700 overflow-auto d-flex flex-column  justify-content-evenly align-items-between">
-        <div className="container p-2 gap-2 d-flex flex-column justify-content-evenly">
+        <div className="container p-2 gap-2  rounded bg-gray-700 border d-flex flex-column justify-content-evenly">
           <div>
             <p>
-              <span className="font-semibold text-gray-400">Username:</span>{" "}
+              <span className="font-bold text-gray-300">Username:</span>{" "}
               {userData?.userName || ""}
             </p>
           </div>
           <div>
             <p>
-              <span className="font-semibold text-gray-400">Email:</span>{" "}
+              <span className="font-bold text-gray-300">Email:</span>{" "}
               {userData?.userEmail || ""}
             </p>
           </div>
           <div>
             <p>
-              <span className="font-semibold text-gray-400">User ID:</span>{" "}
+              <span className="font-bold text-gray-300">User ID:</span>{" "}
               {userData?._id || ""}
             </p>
           </div>
           <div>
             <p>
-              <span className="font-semibold text-gray-400">Groups:</span>{" "}
+              <span className="font-bold text-gray-300">Groups:</span>{" "}
               {groupList || ""}
             </p>
           </div>
           <div>
             <p>
-              <span className="font-semibold text-gray-400">
-                Total Expenses:
-              </span>{" "}
+              <span className="font-bold text-gray-300">Total Expenses:</span>{" "}
               {userData?.userExpenses.join(" ") || ""}
             </p>
           </div>
         </div>
         <div
-          className="container gap-2 p-1 d-flex flex-column justify-content-evenly align-items-center"
+          className="container gap-1 p-0 d-flex flex-column justify-content-evenly align-items-center"
           style={{ height: "-webkit-fill-available" }}
         >
           <div className="container h-30 p-2 d-flex rounded bg-gray-700 justify-content-center align-items-center border">
