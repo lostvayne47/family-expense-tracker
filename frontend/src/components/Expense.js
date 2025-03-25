7db497d3723366c71f78777e664523a49bc8f858
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-26 03:23:47 +0530

    icons resized and layout changed

--- frontend/src/components/Expense.js
+++ frontend/src/components/Expense.js
@@ -3,7 +3,7 @@ import { MdDateRange } from "react-icons/md";
 import { IoPerson } from "react-icons/io5";
 import { MdOutlineDescription } from "react-icons/md";
 import { RiMoneyRupeeCircleFill } from "react-icons/ri";
-import { FaRegEdit } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 
 export default function Expense({ expense }) {
@@ -12,38 +12,38 @@ export default function Expense({ expense }) {
   console.log(expense);
   return (
     <div>
-      <div className="flex justify-end justify-between gap-3 my-1 ">
-        <FaRegEdit
-          className="hover:scale-125 hover:text-green-400 cursor-pointer"
-          size={18}
+      <div className="flex justify-end justify-between gap-3 my-1 items-center">
+        <FaEdit
+          className="p-1 hover:scale-125 hover:text-green-400  cursor-pointer"
+          size={30}
           onClick={handleEdit}
         />
+        <div className="p-3 w-full bg-gray-100 text-black rounded-lg shadow-md flex flex-wrap justify-between items-center text-l hover:bg-cyan-200">
+          <div className="font-semibold w-full md:w-1/4 flex items-center gap-2">
+            <MdDateRange size={24} />
+            <p>{expense.expenseDateFormatted}</p>
+          </div>
+
+          <div className="font-semibold w-full md:w-1/4 flex items-center gap-2">
+            <IoPerson size={24} />
+            <p>{expense.expenseOwner}</p>
+          </div>
+
+          <div className="font-semibold w-full md:w-1/4 flex items-center gap-2">
+            <MdOutlineDescription size={24} />
+            <p>{expense.expenseName}</p>
+          </div>
+
+          <div className="font-semibold w-full md:w-1/4 flex items-center gap-2 text-xl">
+            <RiMoneyRupeeCircleFill size={24} color="black" />
+            <p>{expense.expenseAmount}</p>
+          </div>
+        </div>
         <MdDeleteForever
-          className="hover:scale-125 hover:text-red-400 cursor-pointer"
-          size={18}
+          className="hover:scale-125 hover:text-red-400   cursor-pointer"
+          size={30}
           onClick={handleDelete}
         />
-      </div>
-      <div className="p-3 bg-gray-100 text-black rounded-lg shadow-md flex flex-wrap justify-between items-center text-l hover:bg-cyan-200">
-        <div className="font-semibold w-full md:w-1/4 flex items-center gap-2">
-          <MdDateRange size={30} />
-          <p>{expense.expenseDateFormatted}</p>
-        </div>
-
-        <div className="font-semibold w-full md:w-1/4 flex items-center gap-2">
-          <IoPerson size={30} />
-          <p>{expense.expenseOwner}</p>
-        </div>
-
-        <div className="font-semibold w-full md:w-1/4 flex items-center gap-2">
-          <MdOutlineDescription size={30} />
-          <p>{expense.expenseName}</p>
-        </div>
-
-        <div className="font-semibold w-full md:w-1/4 flex items-center gap-2 text-xl">
-          <RiMoneyRupeeCircleFill size={30} color="black" />
-          <p>{expense.expenseAmount}</p>
-        </div>
       </div>
     </div>
   );
