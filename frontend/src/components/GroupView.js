41fdb8d7777074180f8f9c1e5b17db3e8da35c26
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-27 15:13:16 +0530

    Responsiveness check

--- frontend/src/components/GroupView.js
+++ frontend/src/components/GroupView.js
@@ -6,39 +6,35 @@ export default function GroupView({ showModal, setShowModal, groupData }) {
     <>
       {showModal && (
         <div
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300 z-50 overflow-auto"
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300 z-50"
           onClick={() => setShowModal(false)}
         >
           {/* Modal Content */}
           <div
-            className="bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-3xl max-h-[75vh] overflow-y-auto transition-transform transform scale-95 animate-fadeIn"
+            className="bg-gray-800 p-6 rounded-lg shadow-lg w-75 transition-transform transform scale-95 animate-fadeIn"
             role="dialog"
             aria-modal="true"
             onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
           >
-            {/* Header */}
-            <div className="flex justify-between items-center">
+            <div className="d-flex justify-content-between">
               <h2 className="text-xl font-semibold">{groupData?.groupName}</h2>
               <h2 className="text-xl font-semibold">
                 Members: {groupData?.groupMembers?.length}
               </h2>
             </div>
-
-            {/* Content Section with Scrollable Views */}
-            <div className="mt-2 bg-black flex flex-wrap">
-              <div className="w-full sm:w-1/2 p-2">
-                <div className="max-h-60 overflow-y-auto p-2 bg-gray-900 rounded-md">
-                  <ExpenseList groupId={groupData._id} />
-                </div>
+            <div
+              className="mt-2 bg-black flex"
+              style={{ maxHeight: "80vh", overflowY: "auto" }}
+            >
+              <div className="w-1/2 p-2">
+                <ExpenseList groupId={groupData._id} />
               </div>
-              <div className="w-full sm:w-1/2 p-2">
-                <div className="max-h-60 overflow-y-auto p-2 bg-gray-900 rounded-md">
-                  <ExpenseChart groupId={groupData._id} />
-                </div>
+              <div className="w-1/2 p-2">
+                <ExpenseChart groupId={groupData._id} />
               </div>
             </div>
 
-            {/* Close Button */}
+            {/* Close Modal Button */}
             <div className="mt-4 flex justify-end">
               <button
                 onClick={() => setShowModal(false)}
